771839ba99b5811204ec6726a6bebb05afc19844
Brian Reeves <1949commander@gmail.com>
2021-03-02 14:51:17 -0800

    Assignment Booleans
    Assignment 168
    Insurance approval app

---
+++ Assignment168/Assignment168/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment168
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age? ");
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? True or False");
+            bool hadDui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have? ");
+            int numberSpeedTickets = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(yourAge);
+            //Console.WriteLine(hadDui);
+            //Console.WriteLine(numberSpeedTickets);
+
+            bool yourAgeTrue = yourAge > 15;
+            bool hadDuiTrue = hadDui != true;
+            bool numberSpeedTicketsTrue = numberSpeedTickets <= 3;
+            bool qualifiedTrue = yourAgeTrue && hadDuiTrue && numberSpeedTicketsTrue;
+
+            //Console.WriteLine(yourAgeTrue);
+            //Console.WriteLine(hadDuiTrue);
+            //Console.WriteLine(numberSpeedTicketsTrue);
+
+            Console.WriteLine("Qualified? ");
+            Console.WriteLine(qualifiedTrue);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BooleanLogic164/BooleanLogic164/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 7;
+            int num2 = 12;
+            int num3 = 23;
+            int num4 = 14;
+            int num5 = 8;
+            int num6 = 5;
+
+            bool result = (num1 < num2 && (num3 < num4 || num5 < num6));
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
