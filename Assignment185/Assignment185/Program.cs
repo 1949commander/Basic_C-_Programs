b68bed89dc57473a6534d54b44d923d885d8d4a2
Brian Reeves <1949commander@gmail.com>
2021-03-04 00:26:45 -0800

    If else and branching
    Upload assignment 185

---
+++ Assignment185/Assignment185/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment185
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight: ");
+            int packWeight = Convert.ToInt32(Console.ReadLine());
+            if (packWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width:");
+                int packWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:");
+                int packHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                int packLength = Convert.ToInt32(Console.ReadLine());
+                int totalDim = packWidth + packHeight + packLength;
+
+                if (totalDim > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    int shipQuote = ((packWidth * packHeight * packLength) * packWeight) / 100;
+                    string estQuote = String.Format("Your estimated total for shipping this package is: {0:C}", shipQuote);
+                    Console.WriteLine(estQuote +"\nThank you!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IF_ELSE_STATEMENTS/IF_ELSE_STATEMENTS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_ELSE_STATEMENTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 7;
+            int num2 = 12;
+            int num3 = 2;
+            int num4 = 8;
+
+            string result = num1 > num3 ? "num1 is greater than num3" : "num1 is not greater than num3";
+            Console.WriteLine(result);
+
+            //if (num1 <= num2 && num4 >= num3)
+            //{
+            //    Console.WriteLine("The if statement ran. ");
+                
+            //}
+           
+            //else
+            //{
+            //    Console.WriteLine("The else statement ran.");
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
