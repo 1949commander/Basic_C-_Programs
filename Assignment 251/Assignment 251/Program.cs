647d309903c332c13d6cfff22e3a93dd439fda51
Brian Reeves <1949commander@gmail.com>
2021-03-21 01:17:18 -0700

    Upload assignment 251
    Upload for loop on list of strings

---
+++ Assignment 251/Assignment 251/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment_251
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// PART ONE
+            //// Loop and append
+
+            //string[] citiesArray = { "Boston", "New York", "Atlanta", "Dallas", "Omaha", "Seattle", "San Francisco", "Bosie" };
+
+            //Console.WriteLine("Please enter a name: ");
+            //string nameAdd = Console.ReadLine();
+            //for (int i = 0; i < citiesArray.Length; i++)
+            //{
+            //    citiesArray[i] = citiesArray[i] + " " + nameAdd;
+            //}
+            //foreach (string i in citiesArray)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //// PART TWO
+            //// Creating infinite loop 
+            //// using while loop 
+
+            //int count = 0;
+            //while (true)
+            //{
+            //    // This statement will be printed  
+            //    // infinite times  
+            //    Console.WriteLine("I am stuck in a rut!!! but only this many times = {0}. ", count);
+            //    // Added Count
+            //    count++;
+
+            //    if (count > 10)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //// PART THREE
+            //// Loop and Compare
+
+            //// Step one
+            //int j = 0;
+
+            //do
+            //{
+            //    Console.WriteLine("j = {0}", j);
+            //    j++;
+
+            //    if (j > 5)
+            //        break;
+            //}
+
+            //while (j < 10);
+
+            //// Step two
+            //for (int k = 0; k <= 12; k++)
+            //    Console.WriteLine("Value of k: {0}", k);
+
+            // PART FOUR
+            // STRING LOOP
+            // Create List of Strings
+            List<string> namesList = new List<string>() { "John", "Billy", "James", "Susan", "Mary", "Kathy", "Nitu" };
+
+            // Ask for user input
+            Console.WriteLine("Please enter a name to search for: ");
+
+            //Save User input into a variable
+            string searchName = Console.ReadLine();
+
+            //Loop search
+            for (int m = 0; m < namesList.Count; m++)
+            {
+                if (namesList[m].ToLower() == searchName.ToLower())
+                {
+                    int idx = namesList.IndexOf(searchName);
+                    Console.WriteLine(idx);
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Name you entered is not in the list");
+                }
+                Console.WriteLine(m);
+            }
+
+
+
+
+            //if (m.ToLower() == searchName.ToLower())
+            //{
+            //    int idx = namesList.IndexOf(nameof);
+            //    Console.WriteLine(idx);
+            //}
+
+
+
+            //for (int m = 0; m < namesList.Count; m++)
+            // foreach ( var name in namesList)
+            //    if (searchName = m
+            //    { }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Iteration_Lessons/Iteration_Lessons/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration_Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+            //for (int i = 0; i < testScores.Length; i++)
+            //{
+            //    if (testScores[i] > 85)
+            //    {
+            //        Console.WriteLine("Passsing test score: " + testScores[i]);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //string[] names = { "Jesse", "Erick", "Daniel", "Adam" };
+
+            //for (int j = 0; j < names.Length; j++)
+            //{
+            //                    {
+            //        Console.WriteLine(names[j]);
+            //    }
+            //}
+
+            //Console.ReadLine();
+
+
+            //int[] numberArray = { 300, 75, 420, 380, 225 };
+
+            //for  (int i=0; i < numberArray.Length; i++)
+            //{
+            //    if (numberArray[i] / 2 >= 150)
+            //    {
+            //        Console.WriteLine(numberArray[i] + " divided by 2 is greater than or equal to 150. ");
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //List<int> testScores = new List<int>();
+            //testScores.Add(98);
+            //testScores.Add(99);
+            //testScores.Add(81);
+            //testScores.Add(72);
+            //testScores.Add(70);
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        Console.WriteLine("Passing test score:" + score);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //List<string> names = new List<string>() { "Jesse", "Erik", "Daniel", "Adam" };
+
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+
+            //}
+            //Console.ReadLine();
+
+            List<int> testScores = new List<int>() { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+            List<int> passingScores = new List<int>();
+
+            foreach (int score in testScores)
+            {
+                if (score > 85)
+                {
+                    passingScores.Add(score);
+                }
+
+            }
+
+            Console.WriteLine(passingScores.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
