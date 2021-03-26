d3f0a9d23f68546d2ace92f9fc1ca27f9a3a7263
Brian Reeves <1949commander@gmail.com>
2021-03-26 05:00:45 -0700

    Submit for Step 279
    Updated Twenty One new
    Created Main Method 279

---
+++ Main_Method_Step_279/Main_Method_Step_279/MathOPS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Step_279
+{
+    class MathOPS
+    {
+
+        public MathOPS(int v1, float v2, string v3)
+        {
+            V1 = v1 + 3;
+            V2 = Convert.ToInt32(v2 - 23);
+            V3 = Convert.ToInt32(v3) * 3;
+        }
+
+        public int V1 { get; set; }
+        public int V2 { get; set; }
+        public int V3 { get; set; }
+    }
+}
---
+++ Main_Method_Step_279/Main_Method_Step_279/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Step_279
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inputs
+            int v1 = 8;
+            float v2 = 344.56f;
+            string v3 = "45";
+
+            // Call Methods
+            MathOPS intAddInt = new MathOPS(v1, v2, v3);
+            MathOPS decSubInt = new MathOPS(v1, v2, v3);
+            MathOPS strMultInt = new MathOPS(v1, v2, v3);
+            
+            // Print Results
+            Console.WriteLine("Results of intAddInt:" + intAddInt.V1);
+            Console.WriteLine("Results of decSubInt:" + decSubInt.V2);
+            Console.WriteLine("Results of strMultInt:" + strMultInt.V3);
+            Console.ReadLine();
+        }
+    }
+}
