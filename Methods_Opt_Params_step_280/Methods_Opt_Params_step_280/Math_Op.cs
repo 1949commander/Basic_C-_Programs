1d68898827198b5dfa57dcb6b06fe9bca8a1dac4
Brian Reeves <1949commander@gmail.com>
2021-03-27 15:00:05 -0700

    Submit Methods Step 280
    Upload assignment for review

---
+++ Methods_Opt_Params_step_280/Methods_Opt_Params_step_280/Math_Op.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Opt_Params_step_280
+{
+    class Math_Op
+    {
+        // Method
+        public void MathMethod(int numOne, int numTwoOpt)
+        {
+            // Method Statenebt
+            int resultValue = (numOne + numTwoOpt) * 2;
+            // Print Results
+            Console.WriteLine("Results: {0}", resultValue);
+        }
+    }
+}
---
+++ Methods_Opt_Params_step_280/Methods_Opt_Params_step_280/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Opt_Params_step_280
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Request User Input
+            Console.WriteLine("Enter a whole number");
+            string inputOne = (Console.ReadLine());
+            Console.WriteLine("Optional: Enter another whole number");
+            string inputTwoOpt = (Console.ReadLine());
+
+            // Instanciate
+            Math_Op mathObj = new Math_Op();
+
+            // Call method
+            if (inputTwoOpt == "")
+            {
+                //Convert String Input to int
+                int numOne = Convert.ToInt32(inputOne);
+                int numTwoOpt = 0; // Default Value
+
+                mathObj.MathMethod(numOne, numTwoOpt);
+            }
+
+            else
+            {
+                //Convert String Input to int
+                int numOne = Convert.ToInt32(inputOne);
+                int numTwoInt = Convert.ToInt32(inputTwoOpt);
+
+                mathObj.MathMethod(numOne, numTwoInt);
+            }
+
+                            
+            Console.ReadLine();
+        }
+    }
+}
