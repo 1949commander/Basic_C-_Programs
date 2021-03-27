3e2e94b69e17a1cfd3af0fb51119892fec511f8e
Brian Reeves <1949commander@gmail.com>
2021-03-27 02:10:07 -0700

    Methods_Step_275
    Uploaded New Methods Step 275 Assignment.

---
+++ Methods_Step_275/Methods_Step_275/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Step_275
+{
+    class Math
+    {
+        //Method One
+        public int MathOne(int userInput)
+        {
+            return userInput + 12;
+
+        }
+
+        //Method Two
+        public int MathTwo(int userInput)
+        {
+            return userInput * 7;
+        }
+
+
+        //Method Three
+        public int MathThree(int userInput)
+        {
+            return (userInput * 5) / 2;
+        }
+    }
+}
---
+++ Methods_Step_275/Methods_Step_275/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Step_275
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Input Request
+            Console.WriteLine("Please enter a  whole number to perform math operations on:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Instantiate 
+            Math mathOps = new Math();
+
+            // Call Methods
+            int valueOne = mathOps.MathOne(userInput);
+            int valueTwo = mathOps.MathTwo(userInput);
+            int valueThree = mathOps.MathThree(userInput);
+
+            // Print Results
+            Console.WriteLine("Call MathOne, User Input Value is: {0} Result Value is {1}.",  userInput , valueOne);
+            Console.WriteLine("Call MathTwo, User Input Value is: {0} Result Value is {1}.", userInput , valueTwo);
+            Console.WriteLine("Call MathThree, User Input Value is: {0} Result Value is {1}.", userInput , valueThree);
+
+            Console.ReadLine();
+        }
+    }
+}
