db5f260cbc7d367973ae75bf4fab5811819a9a82
Brian Reeves <1949commander@gmail.com>
2021-03-29 03:39:33 -0700

    Submit Step 281
    Submit assignment Step 281 Methods

---
+++ Method_Class_step-281/Method_Class_step-281/MathOp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_step_281
+{
+    class MathOp
+    {
+        // Create method
+        public void MyMethod(int numOne, int numTwo)
+        {
+            //Math Function
+            int mathOne = numOne * 9;
+
+            //Print Results
+            Console.WriteLine("NumOne times 9 = {0}", mathOne);
+            Console.WriteLine("Value of other number is: {0}", numTwo);
+        }
+    }
+}
---
+++ Method_Class_step-281/Method_Class_step-281/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_step_281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciate Method
+            MathOp Op1 = new MathOp();
+
+            // Call Method
+            Op1.MyMethod(6, 4);
+            Op1.MyMethod(numTwo: 9, numOne: 7);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
