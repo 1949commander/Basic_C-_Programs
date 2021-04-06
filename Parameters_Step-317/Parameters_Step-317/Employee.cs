69d2f33762f5f467ebb37f0cc4b8372de416fe05
Brian Reeves <1949commander@gmail.com>
2021-04-06 04:20:18 -0700

    Upload Step 317
    Upload Step 317

---
+++ Parameters_Step-317/Parameters_Step-317/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Step_317
+{
+    //Create Generic Class
+    public class Employee<T>
+    {
+        //Create Generic List
+        private List<T> things = new List<T>();
+        //Create Generic Property
+        public List<T> Things { get => things; set => things = value; }
+    }
+ 
+}
---
+++ Parameters_Step-317/Parameters_Step-317/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Step_317
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate generic with String
+            Employee<string> names = new Employee<string>();
+
+            names.Things.Add("Joe");
+            names.Things.Add("James");
+            names.Things.Add("Jane");
+            names.Things.Add("Susie");
+
+            foreach(string i in names.Things)
+            {
+                Console.WriteLine(i);
+            }
+
+            //instantiate generic with Int
+            Employee<int> index = new Employee<int>();
+
+            index.Things.Add(1);
+            index.Things.Add(2);
+            index.Things.Add(3);
+            index.Things.Add(4);
+
+            foreach (int i in index.Things)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
