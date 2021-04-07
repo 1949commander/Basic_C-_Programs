1ef5bd33d7c1c71fc385c2baf3cb6e0951f33c4d
Brian Reeves <1949commander@gmail.com>
2021-04-07 03:04:32 -0700

    Step 331 Structure
    Submit Structure assignment
    Submit Twenty One update

---
+++ Struct_Assignment_Step-331/Struct_Assignment_Step-331/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    // Create Struct Number
+    public struct Number
+    {
+        // Create Property Amount
+        public Number (decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public decimal Amount { get; set; }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            // Instantiate Number
+            Number myNum = new Number();
+
+            // Assign Value to Amount
+            myNum.Amount = 3.875m;
+
+            // Print Amount
+            Console.WriteLine("Amount = {0}", myNum.Amount);
+            Console.ReadLine();
+        }
+
+    }
--- TwentyOne_new/TwentyOne_new/Program.cs
+++ TwentyOne_new/TwentyOne_new/Program.cs
@@ -10,15 +10,21 @@ namespace TwentyOne_new
     {
         static void Main(string[] args)
         {
+            Card card1 = new Card();
+            Card card2 = card1;
+            card1.Face = Face.Eight;
+            card2.Face = Face.King;
 
-            Deck deck = new Deck();
-            deck.Shuffle(5);
+            Console.WriteLine(card1.Face);
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            //Deck deck = new Deck();
+            //deck.Shuffle(5);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
     }
