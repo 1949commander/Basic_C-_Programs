4f47a392a424a4847021d7822d58c3c70cb9337b
Brian Reeves <1949commander@gmail.com>
2021-04-12 01:41:55 -0700

    Upload Date Time Step 369
    Upload assignment

---
+++ Date_Time_assignment_Step-369/Date_Time_assignment_Step-369/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time_assignment_Step_369
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get Current Date and Time
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+            // Request hours offset
+            Console.WriteLine("Please enter a whole number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            //Add hours offset to date time
+            DateTime dateTime1 = dateTime.AddHours(hours);
+            //Display new date time with offset
+            Console.WriteLine(dateTime1);
+         
+            Console.ReadLine();
+
+        }
+    }
+}
