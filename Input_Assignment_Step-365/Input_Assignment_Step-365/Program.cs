4e874baeffcfd12e769daf9e11d5a78e37dd1c07
Brian Reeves <1949commander@gmail.com>
2021-04-12 00:32:02 -0700

    Input Assignment Step 365
    Upload assignment

---
+++ Input_Assignment_Step-365/Input_Assignment_Step-365/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_Assignment_Step_365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\breev\OneDrive\Desktop\Basic_C#_Programs\Input_Assignment_Step-365\log.txt", Convert.ToString(number));
+            string readText = File.ReadAllText(@"C:\Users\breev\OneDrive\Desktop\Basic_C#_Programs\Input_Assignment_Step-365\log.txt");
+            Console.WriteLine(readText);
+
+            Console.ReadLine();
+        }
+    }
+}
