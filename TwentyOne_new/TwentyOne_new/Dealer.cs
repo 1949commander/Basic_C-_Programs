c1d50a7f5f48fedb2557f98f4408180fd103e587
Brian Reeves <1949commander@gmail.com>
2021-04-12 00:47:13 -0700

    Date Time Update
    Added a time log

--- TwentyOne_new/TwentyOne_new/Dealer.cs
+++ TwentyOne_new/TwentyOne_new/Dealer.cs
@@ -20,6 +20,7 @@ namespace TwentyOne_new
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\breev\OneDrive\Desktop\Basic_C#_Programs\TwentyOne_new\logs.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
